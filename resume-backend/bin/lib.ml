f52552f29cb8d1183d3d046a7398dc1a55e1f742
cedlemo <cedlemo@gmx.com>
2019-12-28 19:54:14 +0100

    Lib.request_handler: handle json data

--- resume-backend/bin/lib.ml
+++ resume-backend/bin/lib.ml
@@ -4,8 +4,6 @@ open Base
 module String = Caml.String
 
 let invalid_request reqd status body =
-  (* Responses without an explicit length or transfer-encoding are
-     close-delimited. *)
   let headers = Headers.of_list [ "Connection", "close" ] in
   Reqd.respond_with_string reqd (Response.create ~headers status) body
 
@@ -13,31 +11,49 @@ let request_handler reqd =
   let { Request.meth; target; headers; _ } = Reqd.request reqd in
   let build_headers response_body =
         Headers.of_list
-          [ "Content-length", Int.to_string (String.length response_body) ; "connection", "close"]
+          [ "Content-length", Int.to_string (String.length response_body);
+            "Content-Type", "application/json";
+            "connection", "close"]
   in
   match meth with
-  | `GET | `DELETE ->
-    let response_body = Printf.sprintf "%s request on url %s\n" (Method.to_string meth) target in
+  | `GET -> let json_values =
+              `List [
+                `Assoc
+                [
+                    ("id", `String "1");
+                    ("name", `String "todo 1");
+                    ( "description", `String "do this, do that");
+                ];
+                `Assoc
+                [
+                    ("id", `String "2");
+                    ("name", `String "todo 2");
+                    ( "description", `String "do this again, do that again");
+                ]
+              ]
+    in
+    let response_body = Yojson.Basic.to_string json_values in
     let resp_headers = build_headers response_body in
     Reqd.respond_with_string reqd (Response.create ~headers:resp_headers `OK) response_body
   | `POST | `PUT ->
-    let response =
-        let content_type =
-          match Headers.get headers "content-type" with
-          | None   -> "application/octet-stream"
-          | Some x -> x
-        in
-        Response.create ~headers:(Headers.of_list ["content-type", content_type; "connection", "close"]) `OK
-    in
     let request_body  = Reqd.request_body reqd in
-    let response_body = Reqd.respond_with_streaming reqd response in
+    let data = Buffer.create 1024 in
     let rec on_read buffer ~off ~len =
-      Body.write_bigstring response_body buffer ~off ~len;
+      let str = Bigstringaf.substring buffer ~off ~len in
+      let () = Buffer.add_string data str in
       Body.schedule_read request_body ~on_eof ~on_read;
     and on_eof () =
-      Body.close_writer response_body
+      let json = (Buffer.sub data 0 (Buffer.length data)) |> Bytes.to_string  |> Yojson.Basic.from_string in
+      let () = Stdio.print_endline (Yojson.Basic.pretty_to_string json) in
+      let response_body = Printf.sprintf "%s request on url %s\n" (Method.to_string meth) target in
+      let resp_headers = build_headers response_body in
+      Reqd.respond_with_string reqd (Response.create ~headers:resp_headers `OK) response_body
     in
     Body.schedule_read request_body ~on_eof ~on_read
+  | `DELETE ->
+    let response_body = Printf.sprintf "%s request on url %s\n" (Method.to_string meth) target in
+    let resp_headers = build_headers response_body in
+    Reqd.respond_with_string reqd (Response.create ~headers:resp_headers `OK) response_body
   | meth ->
     let response_body =
       Printf.sprintf "%s is not an allowed method\n" (Method.to_string meth)
